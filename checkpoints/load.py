b778a999cabb121feabaa96ca49633de4ccaca47
John Murzaku <jam@lissus.com>
2023-10-19 18:27:22 -0400

    Update load.py

--- checkpoints/load.py
+++ checkpoints/load.py
@@ -5,16 +5,65 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
-model = T5ForConditionalGeneration.from_pretrained("/home/jmurzaku/checkpoints")
-tokenizer = T5Tokenizer.from_pretrained("/home/jmurzaku/checkpoints/tokenizer")
+model = T5ForConditionalGeneration.from_pretrained("PATH_TO_CHECKPOINT")
+tokenizer = T5Tokenizer.from_pretrained("PATH_TO_TOKENIZER")
 
 device = torch.device(f'cuda:{5}')  # Use the appropriate GPU index
 model.to(device)
 model.eval()
 
 max_seq_length = 100  # Define your max sequence length here
-PATH = 'PATH_TO_FILE'
-with open(PATH, 'r') as f:
+
+def extract_parentheses_content(s):
+    stack = []
+    contents = []
+    start = 0
+
+    for i, char in enumerate(s):
+        if char == '(':
+            if not stack:
+                start = i + 1
+            stack.append(i)
+        elif char == ')':
+            if stack:
+                stack.pop()
+                if not stack:
+                    contents.append(s[start:i])
+    return contents
+
+def tree2triplet(tree):
+    triplets = []
+    contents = extract_parentheses_content(tree)
+
+    for values in contents:
+        if 'nest' not in values:
+            values = values.split(' ')
+            triplets.append(
+                ('AUTHOR', values[0], values[-1])
+            )
+        else:
+            top_source = values[:values.find("nest") - 1]
+            values = values[values.find("nest") + len("nest"):]
+            values_nest = extract_parentheses_content(values)
+            for nest_v in values_nest:
+                if 'nest' not in nest_v:
+                    nest_v = nest_v.split(' ')
+                    triplets.append(
+                        (f'AUTHOR_{top_source}', nest_v[0], nest_v[-1])
+                    )
+                else:
+                    top_nest_source = nest_v[:nest_v.find("nest") - 1]
+                    nest_v = nest_v[nest_v.find("nest") + len("nest"):]
+                    values_nest_nest = extract_parentheses_content(nest_v)
+                    for nest_v_nest in values_nest_nest:
+                        nest_v_nest = nest_v_nest.split(' ')
+                        triplets.append(
+                            (f'AUTHOR_{top_source}_{top_nest_source}', nest_v_nest[0], nest_v_nest[-1])
+                        )
+    return triplets
+
+
+with open('/home/jmurzaku/flant5/checkpoints/filtered_negative_case_timeslice2.jsonl', 'r') as f:
     lines = f.read().splitlines()
 
 outputs, targets = [], []
@@ -27,7 +76,7 @@ for line in lines:
 
     for sentence in sentences:
         tokenized_input = tokenizer.encode_plus(
-            "tree to sentence: " + sentence,
+            sentence,
             max_length=max_seq_length,
             pad_to_max_length=True,
             truncation=True,
@@ -42,5 +91,6 @@ for line in lines:
 
         dec = [tokenizer.decode(ids, skip_special_tokens=True) for ids in outs]
         target = [tokenizer.decode(ids, skip_special_tokens=True) for ids in tokenized_input["input_ids"]]
+        print(tree2triplet(dec[0]))
         outputs.extend(dec)
         targets.extend(target)
